2c104eb8462e04ddfea97e20d8b4762137d7eca9
Marc <104395392+marccorv@users.noreply.github.com>
2025-09-30 10:05:48 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,8 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-})
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // No i18n, no rewrites, no redirects for now
+};
 
-const withPWA = require("next-pwa")({
-  dest: "public"
-})
-
-module.exports = withBundleAnalyzer(
-  withPWA({
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost"
-        },
-        {
-          protocol: "http",
-          hostname: "127.0.0.1"
-        },
-        {
-          protocol: "https",
-          hostname: "**"
-        }
-      ]
-    },
-    experimental: {
-      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-    }
-  })
-)
+module.exports = nextConfig;
