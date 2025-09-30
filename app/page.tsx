ce6802eb29bcbe300b0cd23f9bf47b16552fbf8b
Marc <104395392+marccorv@users.noreply.github.com>
2025-09-30 11:10:02 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-// app/page.tsx
-export default function HomePage() {
-  return (
-    <main style={{padding: 24}}>
-      <h1>Chatbot UI</h1>
-      <p>Home is working.</p>
-      <a href="/login">Go to Login</a>
-    </main>
-  );
+export default function HealthPage() {
+  return <pre>OK</pre>;
 }
