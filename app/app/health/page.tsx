b00c0c83b873325e9b42f7252ce7cf9f566d1f86
Marc Corvington <marc@Marcs-MacBook-Pro.local>
2025-10-01 20:05:00 -0400

    Use Pages Router only: remove /app to resolve Next.js conflict

--- app/app/health/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/health/page.tsx
-export default function HealthPage() {
-  return (
-    <main style={{ padding: 24 }}>
-      <pre>ok</pre>
-    </main>
-  );
-}
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function HealthPage() {
-  return <pre>OK</pre>;
-}
